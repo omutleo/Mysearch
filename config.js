3c3758e6fc21b02566381c22f6208541df0ebc42
omutleo <prorokkant@yandex.ru>
2026-03-10 12:10:22 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    GOOGLE_SHEET_CSV: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTgA5Y46KknkOfRv7Dj-1mlnABECey_WYuVZSq6mrstoYnY-WrnH1KZVKafIuZwuXiY00HOt9Uzu91X/pub?gid=11256287&single=true&output=csv',
-    GOOGLE_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxJ2QXm_ahkQ1PzKG8w7bKuSDiiqqQqIwPjelJJrLKLP3ri9OaBIBd5C99pym1HBsoMcA/exec',
-    API_PASSWORD: '20hyptec26',
-    LOGIN_PASSWORD: '20hyptec26'
-};
